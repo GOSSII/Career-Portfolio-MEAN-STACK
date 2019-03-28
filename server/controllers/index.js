94b63e0405359d667ef8b0c9ffa78caa0ba2e4d7
Dipesh Goswami <dipeshpuri9@gmail.com>
2019-03-28 14:22:43 -0400

    added login and registration functionality

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,47 +1,126 @@
 let express = require('express');
 let router = express.Router();
+let mongoose = require("mongoose");
+let passport = require("passport");
+
+// define the User Model
+let userModel = require("../models/user");
+let User = userModel.User; // alias
 
 module.exports.displayHomePage = (req, res, next) => {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: 'Home' ,
+  displayName: req.user ? req.user.displayName : ""});
 };
 
 module.exports.displayAboutPage = (req, res, next) => {
-  res.render('about', { title: 'About' });
+  res.render('about', { title: 'About' ,
+  displayName: req.user ? req.user.displayName : ""});
 };
 
 module.exports.displayProjectPage = (req, res, next) => {
-  res.render('projects/index', { title: 'Products' });
+  res.render('projects/index', { title: 'Products' ,
+  displayName: req.user ? req.user.displayName : ""});
 }
 
 module.exports.displayServicesPage = (req, res, next) => {
-  res.render('services', { title: 'services' });
+  res.render('services', { title: 'services' ,
+  displayName: req.user ? req.user.displayName : ""});
 }
 
 module.exports.displayContactPage = (req, res, next) => {
-  res.render('contact/index', { title: 'Contact' });
+  res.render('contact/index', { title: 'Contact' ,
+  displayName: req.user ? req.user.displayName : ""});
 }
 
 module.exports.displayToDoPage = (req, res, next) => {
-  res.render('todo/index', { title: 'todo' });
+  res.render('todo/index', { title: 'todo' ,
+  displayName: req.user ? req.user.displayName : ""});
 }
 
 module.exports.displayLoginPage = (req, res, next) => {
-  // TODO
+    // check if user is already logged in
+    if (!req.user) {
+      res.render("auth/login", {
+        title: "Login",
+        messages: req.flash("loginMessage"),
+        displayName: req.user ? req.user.displayName : ""
+      });
+    } else {
+      return res.redirect("/");
+    }
 }
 
 module.exports.processLoginPage = (req, res, next) => {
-  // TODO
+  passport.authenticate('local', 
+  (err, user, info) => {
+    // server error?
+    if(err) {
+      return next(err);
+    }
+    // is there a user login error?
+    if(!user) {
+      req.flash("loginMessage", "Authentication Error");
+      return res.redirect('/login');
+    }
+    req.logIn(user, (err) => {
+      // server error?
+      if(err) {
+        return next(err);
+      }
+      return res.redirect('/todo');
+    });
+  })(req, res, next);
 }
 
 module.exports.displayRegisterPage = (req, res, next) => {
-  // TODO
-}
+  if (!req.user) {
+    res.render("auth/register", {
+      title: "Register",
+      messages: req.flash("registerMessage"),
+      displayName: req.user ? req.user.displayName : ""
+    });
+  } else {
+    return res.redirect("/");
+  }
+};
 
 module.exports.processRegisterPage = (req, res, next) => {
-  // TODO
-}
+  // define a new user object
+  let newUser = new User({
+    username: req.body.username,
+    //password: req.body.password
+    email: req.body.email,
+    displayName: req.body.displayName
+  });
+
+  User.register(newUser, req.body.password, (err) => {
+    if (err) {
+      console.log("Error: Inserting New User");
+      if (err.name == "UserExistsError") {
+        req.flash(
+          "registerMessage",
+          "Registration Error: User Already Exists!"
+        );
+        console.log("Error: User Already Exists!");
+      }
+      return res.render("auth/register", {
+        title: "Register",
+        messages: req.flash("registerMessage"),
+        displayName: req.user ? req.user.displayName : ""
+      });
+    } else {
+      // if no error exists, then registration is successful
+
+      // redirect the user
+      return passport.authenticate("local")(req, res, () => {
+        res.redirect("/todo");
+      });
+    }
+  });
+};
 
 module.exports.performLogout = (req, res, next) => {
-  // TODO
+  req.logout();
+  res.redirect("/");
 }
 
