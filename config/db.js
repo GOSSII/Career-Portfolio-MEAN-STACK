bc08ca9aa9adf7fc4f260bdb918c7b44fd69fc98
Dipesh Goswami <dipeshpuri9@gmail.com>
2019-03-28 09:25:47 -0400

    setup environment

---
+++ config/db.js
@@ -0,0 +1,11 @@
+// Name: Dipeshpuri Goswami
+// Id: 300984229 
+// Date 16, Feb 2019 
+
+module.exports = {
+    // local MongoDb deployment
+"URI": "mongodb://localhost/test"
+   // "URI" : "mongodb://<Dipesh>:<Dipesh@123>@ds058548.mlab.com:58548/comp308-w2019-lession4a"
+   //"URI" : "mongodb://thomas:a123456@ds044989.mlab.com:44989/comp308-w2019-lesson4a"
+   //"URI": "mongodb://Dipesh:Dipesh123@ds121105.mlab.com:21105/dipesh"
+}
---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+// Name: Dipeshpuri Goswami
+// Id: 300984229 
+// Date 16, Feb 2019 
+
+/* Custome javascript goes here */
+// IIFE
+(   function(){
+    function Start(){
+        console.log("%c App Started..")
+    }
+
+    window.addEventListener("load",Start);
+})();
